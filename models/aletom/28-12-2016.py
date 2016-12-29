a55ab8a0b94912fd3ebcabd866a02f6f238073b3
Tom Arnoldussen <tom.arnoldussen@gmail.com>
2016-12-29 19:25:22 +0100

    New

--- models/aletom/28-12-2016.py
+++ models/aletom/28-12-2016.py
@@ -5,9 +5,11 @@ from sklearn import linear_model
 from sklearn.model_selection import GridSearchCV
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, f_classif
 import sklearn.metrics as metrics
 import matplotlib.pyplot as plt
 from collections import Counter
+from sklearn.metrics import accuracy_score
 
 # read the file into a dataframe
 X = pd.read_csv('data/trainremake.csv')
@@ -23,10 +25,11 @@ for col_name in X.columns:
     if X[col_name].dtypes == 'object':
         unique_cat = len(X[col_name].unique())
         var.append(col_name)
-        print("Feature '{col_name}' has {unique_cat} unique categories".format(col_name=col_name, unique_cat=unique_cat))
+        # print("Feature '{col_name}' has {unique_cat} unique categories".format(col_name=col_name, unique_cat=unique_cat))
 
+print var
 # Create a list of features to dummy
-todummy_list = ['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition', 'StreetName']
+todummy_list = ['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition']
 
 
 # Function to dummy all the categorical variables used for modeling
@@ -45,6 +48,8 @@ X = dummy_df(X, todummy_list)
 
 # How much of our data is missing?
 # print X.isnull().sum().sort_values(ascending=False).head()
+
+# print X.columns
 
 #Imputer in sklearn.preprocessing
 from sklearn.preprocessing import Imputer
@@ -72,13 +77,15 @@ ests_labels = np.array(['Linear', 'Ridge', 'Lasso', 'ElasticNet', 'BayesRidge', 
 errvals = np.array([])
 
 X_train, X_test, y_train, y_test = train_test_split(X.drop(['SalePrice'], axis=1),
-                                                    X.SalePrice, test_size=0.2, random_state=100)
+                                                    X.SalePrice, test_size=0.2, random_state=20)
 
 for e in ests:
     e.fit(X_train, y_train)
     this_err = metrics.median_absolute_error(y_test, e.predict(X_test))
     #print "got error %0.2f" % this_err
     errvals = np.append(errvals, this_err)
+
+print errvals
 
 pos = np.arange(errvals.shape[0])
 srt = np.argsort(errvals)
@@ -87,3 +94,74 @@ plt.bar(pos, errvals[srt], align='center')
 plt.xticks(pos, ests_labels[srt])
 plt.xlabel('Estimator')
 plt.ylabel('Median Absolute Error')
+plt.show()
+
+
+##################
+# model ensemble #
+##################
+
+n_est = 500
+
+tuned_parameters = {
+    "n_estimators": [ n_est ],
+    "max_depth" : [ 4 ],
+    "learning_rate": [ 0.01 ],
+    "min_samples_split" : [ 2 ],
+    "loss" : [ 'ls', 'lad' ]
+}
+
+
+gbr = ensemble.GradientBoostingRegressor()
+clf = GridSearchCV(gbr, cv=3, param_grid=tuned_parameters,
+        scoring='neg_median_absolute_error')
+preds = clf.fit(X_train, y_train)
+best = clf.best_estimator_
+
+
+# plot error for each round of boosting
+test_score = np.zeros(n_est, dtype=np.float64)
+
+train_score = best.train_score_
+for i, y_pred in enumerate(best.staged_predict(X_test)):
+    test_score[i] = best.loss_(y_test, y_pred)
+    accuracy = accuracy_score(y_test, predictions)
+	print("Accuracy: %.2f%%" % (accuracy * 100.0))
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.plot(np.arange(n_est), train_score, 'darkblue', label='Training Set Error')
+plt.plot(np.arange(n_est), test_score, 'red', label='Test Set Error')
+plt.legend(loc='upper right')
+plt.xlabel('Boosting Iterations')
+plt.ylabel('Least Absolute Deviation')
+plt.show()
+
+
+feature_importance = clf.best_estimator_.feature_importances_
+feature_importance = 100.0 * (feature_importance / feature_importance.max())
+sorted_idx = np.argsort(feature_importance)
+pos = np.arange(sorted_idx.shape[0]) + 2
+pvals = feature_importance[sorted_idx]
+pcols = X_train.columns[sorted_idx]
+plt.figure(figsize=(32,48))
+plt.barh(pos, pvals, align='center')
+plt.yticks(pos, pcols)
+plt.xlabel('Relative Importance')
+plt.title('Variable Importance')
+plt.show()
+
+
+
+# test = pd.read_csv('data/testremake.csv')
+# import csv
+# with open('output.csv', 'w') as outcsv: 
+#     writer = csv.writer(outcsv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+#     writer.writerow(['Id', 'SalePrice'])
+#     dataPrediction = clf.predict(test)
+#     print dataPrediction
+#     for i in xrange(0, len(test)):
+#         dataId = test['Id'][i]
+#         if dataPrediction[i] < 0:
+#             writer.writerow([dataId, 0])
+#         else:
+#             writer.writerow([dataId, dataPrediction[i]])
