58ef1e2b76b681d3ba68c4ffcb78e45811edfce0
Tom Arnoldussen <tom.arnoldussen@gmail.com>
2016-12-29 13:39:28 +0100

    New python shit

--- models/aletom/28-12-2016.py
+++ models/aletom/28-12-2016.py
@@ -10,13 +10,56 @@ import matplotlib.pyplot as plt
 from collections import Counter
 
 # read the file into a dataframe
-df = pd.read_csv('../../data/28-12-2016.csv')
+X = pd.read_csv('data/trainremake.csv')
 
-len(df.index)
+#######################
+# basic data cleaning #
+#######################
+
+#### Decide which categorical variables you want to use in model
+# Make an array of it
+var = []
+for col_name in X.columns:
+    if X[col_name].dtypes == 'object':
+        unique_cat = len(X[col_name].unique())
+        var.append(col_name)
+        print("Feature '{col_name}' has {unique_cat} unique categories".format(col_name=col_name, unique_cat=unique_cat))
+print var
 
 
-nb_counts = Counter(df.StreetName)
-print nb_counts
-tdf = pd.DataFrame.from_dict(nb_counts, orient='index').sort_values(by=0)
-tdf.plot.bar()
+# Create a list of features to dummy
+todummy_list = ['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition', 'StreetName']
+axs = pd.scatter_matrix(X[X.columns], figsize=(12, 12), c='red')
 
+# Function to dummy all the categorical variables used for modeling
+def dummy_df(df, todummy_list):
+    for x in todummy_list:
+        dummies = pd.get_dummies(df[x], prefix=x, dummy_na=False)
+        df = df.drop(x, 1)
+        df = pd.concat([df, dummies], axis=1)
+    return df
+
+
+X = dummy_df(X, todummy_list)
+
+axs = pd.scatter_matrix(X[X.columns], figsize=(12, 12), c='red')
+
+# print(X.head(5))
+
+#### Handling missing data
+
+# How much of our data is missing?
+# print X.isnull().sum().sort_values(ascending=False).head()
+
+#Imputer in sklearn.preprocessing
+from sklearn.preprocessing import Imputer
+
+imp = Imputer(missing_values='NaN', strategy='median', axis=0)
+imp.fit(X)
+X = pd.DataFrame(data=imp.transform(X) , columns=X.columns)
+
+# Now check again to see if you still have missing data
+print X.isnull().sum().sort_values(ascending=False).head()
+
+#### Remove and detect outliers
+#http://scikit-learn.org/stable/modules/outlier_detection.html
