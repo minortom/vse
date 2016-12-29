0a4db1c143ee41452eb57ddbc3f99432d2e435b2
Tom Arnoldussen <tom.arnoldussen@gmail.com>
2016-12-29 14:45:12 +0100

    Cleaning data

--- models/aletom/28-12-2016.py
+++ models/aletom/28-12-2016.py
@@ -24,12 +24,10 @@ for col_name in X.columns:
         unique_cat = len(X[col_name].unique())
         var.append(col_name)
         print("Feature '{col_name}' has {unique_cat} unique categories".format(col_name=col_name, unique_cat=unique_cat))
-print var
-
 
 # Create a list of features to dummy
 todummy_list = ['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition', 'StreetName']
-axs = pd.scatter_matrix(X[X.columns], figsize=(12, 12), c='red')
+
 
 # Function to dummy all the categorical variables used for modeling
 def dummy_df(df, todummy_list):
@@ -42,9 +40,6 @@ def dummy_df(df, todummy_list):
 
 X = dummy_df(X, todummy_list)
 
-axs = pd.scatter_matrix(X[X.columns], figsize=(12, 12), c='red')
-
-# print(X.head(5))
 
 #### Handling missing data
 
